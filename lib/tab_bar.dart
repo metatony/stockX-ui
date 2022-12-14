04719f6407b782179ae2383b02f85a9ec8efdcae
alexacolbe <58854674+alexacolbe@users.noreply.github.com>
2022-12-14 01:21:30 +0100

    added tab bar for signIn/logIn

---
+++ lib/tab_bar.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:stock_x/Sign_up.dart';
+import 'package:stock_x/log_in.dart';
+
+class TabBarr extends StatelessWidget {
+  const TabBarr({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child:
+                Image(height: 100, image: AssetImage('images/stockx text.png')),
+          ),
+          elevation: 1,
+          backgroundColor: Colors.white,
+          bottom: const TabBar(
+            labelStyle: TextStyle(fontWeight: FontWeight.w500),
+            indicatorColor: Colors.black,
+            tabs: [
+              Tab(
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              Tab(
+                child: Text(
+                  'Log In',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            SignUp(),
+            Login(),
+          ],
+        ),
+      ),
+    );
+  }
+}
