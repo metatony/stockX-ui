a596636157faedaf3d7f759a779acc23bd1121de
alexacolbe <58854674+alexacolbe@users.noreply.github.com>
2022-12-14 01:22:07 +0100

    modified pages for tab bar

--- lib/Sign_up.dart
+++ lib/Sign_up.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:stock_x/components/loginbuts.dart';
 import 'package:stock_x/components/text_field.dart';
@@ -14,119 +13,73 @@ class SignUp extends StatelessWidget {
       backgroundColor: Colors.white,
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const Center(
-                child: Image(
-                  height: 100,
-                  image: AssetImage('images/stockx text.png'),
+          child: Container(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              children: [
+                const TextfieldArea(),
+                const Text(
+                  'At least 8 characters, 1 uppercase letter, 1 number & 1 symbol',
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(15),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        TextButton(
-                          onPressed: () {},
-                          child: const Text(
-                            'Sign Up',
-                            style: TextStyle(
-                                color: Colors.black87,
-                                letterSpacing: 1.5,
-                                fontWeight: FontWeight.w700,
-                                fontSize: 14),
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (BuildContext context) {
-                                return Login();
-                              }),
-                            );
-                          },
-                          child: const Text(
-                            'Log In',
-                            style: TextStyle(
-                                color: Colors.black87,
-                                letterSpacing: 1.5,
-                                fontWeight: FontWeight.w700,
-                                fontSize: 14),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const TextfieldArea(),
-                    const Text(
-                      'At least 8 characters, 1 uppercase letter, 1 number & 1 symbol',
-                      style:
-                          TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    MaterialButton(
-                      elevation: 0,
-                      //padding: EdgeInsets.only(top: 18, bottom: 18),
-                      minWidth: double.infinity,
-                      height: 55,
-                      color: Colors.black,
-                      onPressed: () {},
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(6)),
-                      child: const Text(
-                        'Sign Up',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16),
+                const SizedBox(
+                  height: 10,
+                ),
+                MaterialButton(
+                  elevation: 0,
+                  //padding: EdgeInsets.only(top: 18, bottom: 18),
+                  minWidth: double.infinity,
+                  height: 55,
+                  color: Colors.black,
+                  onPressed: () {},
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(6)),
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const SignLoginButtons(),
+                const SizedBox(
+                  height: 15,
+                ),
+                RichText(
+                  text: const TextSpan(
+                    style: TextStyle(fontSize: 12, color: Colors.black54),
+                    children: [
+                      TextSpan(
+                        text: 'By signing up, you agree to the ',
                       ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const SignLoginButtons(),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    RichText(
-                      text: const TextSpan(
-                          style: TextStyle(fontSize: 12, color: Colors.black54),
-                          children: [
-                            TextSpan(
-                              text: 'By signing up, you agree to the ',
-                            ),
-                            TextSpan(
-                                text: 'Terms of Service ',
-                                style: TextStyle(
-                                    color: Colors.black87,
-                                    fontWeight: FontWeight.bold)),
-                            TextSpan(
-                              text: 'and ',
-                            ),
-                            TextSpan(
-                                text: 'Privacy Policy',
-                                style: TextStyle(
-                                    color: Colors.black87,
-                                    fontWeight: FontWeight.bold)),
-                          ]),
-                    ),
-                  ],
+                      TextSpan(
+                          text: 'Terms of Service ',
+                          style: TextStyle(
+                              color: Colors.black87,
+                              fontWeight: FontWeight.bold)),
+                      TextSpan(
+                        text: 'and ',
+                      ),
+                      TextSpan(
+                          text: 'Privacy Policy',
+                          style: TextStyle(
+                              color: Colors.black87,
+                              fontWeight: FontWeight.bold)),
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(
-                height: 50,
-              )
-            ],
+                const SizedBox(
+                  height: 50,
+                ),
+              ],
+            ),
           ),
         ),
       ),
-
     );
   }
 }
--- lib/log_in.dart
+++ lib/log_in.dart
@@ -12,147 +12,103 @@ class Login extends StatelessWidget {
       backgroundColor: Colors.white,
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              const Center(
-                child: Image(
-                  height: 100,
-                  image: AssetImage('images/stockx text.png'),
+          child: Container(
+            padding: const EdgeInsets.all(15),
+            child: Column(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(bottom: 8.0),
+                  child: const TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 226, 226, 226),
+                        ),
+                      ),
+                      labelText: 'Username',
+                    ),
+                  ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.all(15),
-                child: Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                Container(
+                  margin: const EdgeInsets.only(bottom: 8.0),
+                  child: const TextField(
+                    obscureText: true,
+                    obscuringCharacter: '*',
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                          color: Color.fromARGB(255, 226, 226, 226),
+                        ),
+                      ),
+                      labelText: 'Password',
+                    ),
+                  ),
+                ),
+                const Text(
+                  'At least 8 characters, 1 uppercase letter, 1 number & 1 symbol',
+                  style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                MaterialButton(
+                  elevation: 0,
+                  //padding: EdgeInsets.only(top: 18, bottom: 18),
+                  minWidth: double.infinity,
+                  height: 55,
+                  color: Colors.black,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (BuildContext context) {
+                        return const HomeScreen();
+                      }),
+                    );
+                  },
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(5)),
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const SignLoginButtons(),
+                const SizedBox(
+                  height: 15,
+                ),
+                RichText(
+                  text: const TextSpan(
+                      style: TextStyle(fontSize: 12, color: Colors.black54),
                       children: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: const Text(
-                            'Sign Up',
+                        TextSpan(
+                          text: 'By signing up, you agree to the ',
+                        ),
+                        TextSpan(
+                            text: 'Terms of Service ',
                             style: TextStyle(
                                 color: Colors.black87,
-                                letterSpacing: 1.5,
-                                fontWeight: FontWeight.w700,
-                                fontSize: 14),
-                          ),
+                                fontWeight: FontWeight.bold)),
+                        TextSpan(
+                          text: 'and ',
                         ),
-                        TextButton(
-                          onPressed: () {},
-                          child: const Text(
-                            'Log In',
+                        TextSpan(
+                            text: 'Privacy Policy',
                             style: TextStyle(
                                 color: Colors.black87,
-                                letterSpacing: 1.5,
-                                fontWeight: FontWeight.w700,
-                                fontSize: 14),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(bottom: 8.0),
-                      child: const TextField(
-                        decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Color.fromARGB(255, 226, 226, 226),
-                            ),
-                          ),
-                          labelText: 'Username',
-                        ),
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(bottom: 8.0),
-                      child: const TextField(
-                        obscureText: true,
-                        obscuringCharacter: '*',
-                        decoration: InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(
-                              color: Color.fromARGB(255, 226, 226, 226),
-                            ),
-                          ),
-                          labelText: 'Password',
-                        ),
-                      ),
-                    ),
-                    const Text(
-                      'At least 8 characters, 1 uppercase letter, 1 number & 1 symbol',
-                      style:
-                          TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    MaterialButton(
-                      elevation: 0,
-                      //padding: EdgeInsets.only(top: 18, bottom: 18),
-                      minWidth: double.infinity,
-                      height: 55,
-                      color: Colors.black,
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (BuildContext context) {
-                            return const HomeScreen();
-                          }),
-                        );
-                      },
-                      shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(5)),
-                      child: const Text(
-                        'Sign Up',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16),
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const SignLoginButtons(),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    RichText(
-                      text: const TextSpan(
-                          style: TextStyle(fontSize: 12, color: Colors.black54),
-                          children: [
-                            TextSpan(
-                              text: 'By signing up, you agree to the ',
-                            ),
-                            TextSpan(
-                                text: 'Terms of Service ',
-                                style: TextStyle(
-                                    color: Colors.black87,
-                                    fontWeight: FontWeight.bold)),
-                            TextSpan(
-                              text: 'and ',
-                            ),
-                            TextSpan(
-                                text: 'Privacy Policy',
-                                style: TextStyle(
-                                    color: Colors.black87,
-                                    fontWeight: FontWeight.bold)),
-                          ]),
-                    ),
-                  ],
+                                fontWeight: FontWeight.bold)),
+                      ]),
                 ),
-              ),
-              const SizedBox(
-                height: 50,
-              )
-            ],
+              ],
+            ),
           ),
         ),
       ),
-
     );
   }
 }
